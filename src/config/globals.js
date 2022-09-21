7990d8fcd8fc035b4f6959abb372862eb291f72c
Polibyus <89615619+Polibyus@users.noreply.github.com>
2022-09-21 08:30:23 -0300

    Update globals.js

--- src/config/globals.js
+++ src/config/globals.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 
 module.exports = {
-  MONGO_URI: process.env.MONGO_URI || 'mongodb+srv://JuanchiFP:juanchi123@cluster0.jaeglry.mongodb.net/?retryWrites=true&w=majority',
+  MONGO_URI: process.env.MONGO_URI || '',
   TIEMPO_EXPIRACION: process.env.TIEMPO_EXPIRACION || 300000,
   PORT: process.env.PORT || '3000'
 }
